c133a7c34869f75ff797fae9e6e4b1e228d063a6
saikrishna-2512 <8812sai@gmai.com>
2026-07-28 00:49:41 +0530

    Add ui directory files and update deploy workflow

---
+++ ui/src/App.js
@@ -0,0 +1,15 @@
+import Dashboard from './components/Dashboard';
+import Image from './components/Image';
+import Report from './components/Report';
+import { BrowserRouter as Router, Routes, Route,Link} from "react-router-dom";
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/images" element={<Image />} />
+        <Route path="/reports" element={<Report />} />
+      </Routes>
+    </Router>
+  );
+}
